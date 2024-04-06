ed04e1bde7ddad01f6b10a153ee7f5c840366bd7
StephenPhilipOFlynn <soflynn@tcd.ie>
2024-04-06 22:43:07 +0000

    add basic app.py page and check running

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title('Mildew in Cherry leaves')
+
+st.write("This is a simple dashboard, to show conclusions of the machine learning task on distinguishing cherry leaves with mildew")
+
+st.write("Project Summary")
+
+st.info(
+    "**General Information**\n"
+    "**Project Dataset**\n"
+    "*   \n"  
+    " \n"  
+    "a.")  
+
+st.write(
+    "* For additional information, please visit and **read** the "
+    "[Project README file](https://github.com/StephenPhilipOFlynn/project5-mildrew).")
+
+st.success(
+    "The project has 2 business requirements:\n"
+    "1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew."
+    "2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew."
+)
