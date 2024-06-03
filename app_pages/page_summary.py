275fc11d51e1d41014fc7a9b172e907259c21676
Stephen OFlynn <soflynn@tcd.ie>
2024-06-03 17:29:47 +0000

    adjust streamlit overview summary page

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,12 +1,10 @@
-"""
 import streamlit as st
 import matplotlib.pyplot as plt
 
+"""
 st.title('Mildew in Cherry leaves')
 
-st.write("This is a dashboard overview, of a machine learning task on distinguishing cherry leaves with mildew")
-
-st.write("Project Summary")
+st.write("## Project Summary")
 
 st.info(
     "**General Information**\n"
@@ -22,6 +20,6 @@ st.write(
 st.success(
     "The project has 2 business requirements:\n"
     "1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew."
-    "2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew."
+    "2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew fungus."
 )
 """
