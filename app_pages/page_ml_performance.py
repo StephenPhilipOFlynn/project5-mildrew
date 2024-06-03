853a6575e369048bfa5d676ceb7400ce4667ab9a
Stephen OFlynn <soflynn@tcd.ie>
2024-06-03 20:20:12 +0000

    add ml performance page

---
+++ app_pages/page_ml_performance.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
+
+
+def page_ml_performance_metrics():
+    version = 'v1'
+
+    st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+    ## add functions for model accuracy and loss
