247bc1883ad386ee389609d624ae8523ae5c5f0b
Stephen OFlynn <soflynn@tcd.ie>
2024-06-09 20:25:13 +0000

    build dashboard out from page_summary page to consolidate into less pages

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -1,25 +1,29 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
 
-"""
+
 st.title('Mildew in Cherry leaves')
 
 st.write("## Project Summary")
 
 st.info(
-    "**General Information**\n"
-    "**Project Dataset**\n"
-    "*   \n"  
-    " \n"  
-    "a.")  
+    "General Information\n\n"
+    "**Project Dataset is taken from Kaggle. The data set is taken under licence from Code Institute.\n\n"
+    "https://www.kaggle.com/datasets/codeinstitute/cherry-leaves")
 
 st.write(
-    "* For additional information, please visit and **read** the "
+    "For additional information, please visit and **read** the "
     "[Project README file](https://github.com/StephenPhilipOFlynn/project5-mildrew).")
 
 st.success(
-    "The project has 2 business requirements:\n"
-    "1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew."
+    "The project has 2 business requirements:\n\n"
+    "1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf from one with powdery mildew.\n\n"
     "2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew fungus."
 )
-"""
+
+st.write("### Project Hypothesis and Validation")
+
+st.success(
+        "We hypothesise that mildew-infected leaves exhibit distinct visual characteristics, that can be distinguished from healthy plants using image classification machine learning techniques.")
