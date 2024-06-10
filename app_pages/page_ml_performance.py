6cbe49767c7c011ba46308db5ab32b0b28f0fb7d
Stephen OFlynn <soflynn@tcd.ie>
2024-06-10 23:22:18 +0000

    create data management file, adjust ml performance to d dashboardisplay glay perf metric

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -2,7 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.image import imread
-# from src.machine_learning.evaluate_clf import load_test_evaluation
+from src.machine_learning.evaluate import load_test_evaluation
 
 
 def page_ml_performance_metrics():
---
+++ src/data_management.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import os
+import base64
+from datetime import datetime
+import joblib
+
+def load_pkl_file(file_path):
+    return joblib.load(filename=file_path)
