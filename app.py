75b92199e4ddb4077a0548e3281ed259efd032e3
Stephen OFlynn <soflynn@tcd.ie>
2024-06-10 00:00:30 +0000

    add mildew and healthy leaf visualiser dashboard page

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ st.set_page_config(page_title="Mildew Detector", layout="centered")
 from app_pages.page_summary import page_summary_body
 from app_pages.page_summary import page_summary_hypothesis
 from app_pages.page_ml_performance import page_ml_performance_metrics
+from app_pages.page_visualiser import page_visualiser_body
 
 
 #from app_pages.page_summary import page_mildew_visualiser_body
@@ -20,7 +21,7 @@ app = MultiPage(app_name= "Mildew Detector") # Create an instance of the app
 app.add_page("Project Summary", page_summary_body)
 app.add_page("Project Hypothesis", page_summary_hypothesis)
 app.add_page("ML Performance Metrics", page_ml_performance_metrics)
-
+app.add_page("Visualiser", page_visualiser_body)
 
 #app.add_page("Visualiser", page_visualiser_body)
 #app.add_page("Mildew Detection", page_mildew_detector_body)
--- app_pages/page_visualiser.py
+++ app_pages/page_visualiser.py
@@ -9,7 +9,73 @@ from matplotlib.image import imread
 import itertools
 import random
 
-"""
+def page_visualiser_body():
+    st.write("### Mildew Visualiser")
+    st.info(
+        f"* The client is interested to have a study to visually differentiate "
+        f"a healthy leaf and mildew infected leaf.")
+    
+    version = 'v1'
+    
+    if st.checkbox("Differences between average mildew and average healthy leaves"):
+          diff_between_avgs = plt.imread(f"outputs/{version}/avg_diff.png")
+
+          st.warning(
+            f"We notice that while some visual difference is noticeable between a healthy leaf and a poweder mildew infected leaf")
+            #show difference
+          st.image(diff_between_avgs, caption='Difference between average images')
+
+    if st.checkbox("Image Montage"): 
+      st.write("* To refresh the montage, click on 'Create Montage' button")
+      my_data_dir = 'split_data'
+      labels = os.listdir(my_data_dir + '/validation')
+      label_to_display = st.selectbox(label="Select label", options=labels, index=0)
+      if st.button("Create Montage"):      
+        image_montage(dir_path= my_data_dir + '/validation',
+                      label_to_display=label_to_display,
+                      nrows=8, ncols=3, figsize=(10,25))
+      st.write("---")
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(15,10)):
+  sns.set_style("white")
+  labels = os.listdir(dir_path)
+
+  # subset the class you are interested to display
+  if label_to_display in labels:
+
+    # checks if your montage space is greater than subset size
+    # how many images in that folder
+    images_list = os.listdir(dir_path+'/'+ label_to_display)
+    if nrows * ncols < len(images_list):
+      img_idx = random.sample(images_list, nrows * ncols)
+    else:
+      print(
+          f"Decrease nrows or ncols to create your montage. \n"
+          f"There are {len(images_list)} in your subset. "
+          f"You requested a montage with {nrows * ncols} spaces")
+      return
+    
+
+    # create list of axes indices based on nrows and ncols
+    list_rows= range(0,nrows)
+    list_cols= range(0,ncols)
+    plot_idx = list(itertools.product(list_rows,list_cols))
 
 
-"""
+    # create a Figure and display images
+    fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+    for x in range(0,nrows*ncols):
+      img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+      img_shape = img.shape
+      axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+      axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+      axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+      axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+    plt.tight_layout()
+    
+    st.pyplot(fig=fig)
+    # plt.show()
+
+  else:
+    print("The label you selected doesn't exist.")
+    print(f"The existing options are: {labels}")
