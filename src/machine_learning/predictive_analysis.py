2198f565643d5f34d6acbd4a3615e13327755575
Stephen OFlynn <soflynn@tcd.ie>
2024-06-18 09:44:22 +0000

    remove unnecesary imports and correct bugs and finish load model function

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-import plotly.express as px
 from tensorflow.keras.models import load_model
 from PIL import Image
 from src.data_management import load_pkl_file
@@ -14,7 +12,7 @@ def resize_input_image(img, version):
     """ 
     image_shape = load_pkl_file(file_path=f"outputs/{version}/image_shape.pk1")
     img_resized = img.resize((image_shape[1], image_shape[0]), Image.ANTIALIAS)
-    my_image = np.expland_dims(img_resized, axis=0) / 255
+    my_image = np.expand_dims(img_resized, axis=0) / 255
 
     return my_image
 
@@ -24,5 +22,10 @@ def load_model_and_predict(my_image, version):
     """
     model = load_model(f"outputs/{version}/mildew_detector_model100.h5")
 
-    ### finish this function
+    target_map = {v:k for k, v in {'Healthy': 0, 'Unhealthy': 1}.items()}
+    pred_class - target_map[pred_proba > 0.5]
+    if pred_class == target_map[0]:
+        pred_proba = 1 - pred_proba
+    
+    return pred_proba, pred_class
 
